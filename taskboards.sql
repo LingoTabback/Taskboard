911db1de94d72922478283a902785bce9a5625fd
Lucca Troll <lucca-troll@web.de>
2024-02-04 13:41:40 +0100

    Update taskboards.sql

--- taskboards.sql
+++ taskboards.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 08. Jan 2024 um 15:43
+-- Erstellungszeit: 03. Feb 2024 um 22:07
 -- Server-Version: 10.4.28-MariaDB
 -- PHP-Version: 8.2.4
 
@@ -59,7 +59,12 @@ CREATE TABLE `personen` (
 --
 
 INSERT INTO `personen` (`id`, `vorname`, `name`, `email`, `passwort`) VALUES
-(1, 'Suse', 'Tran', 'st@mail.de', '123456');
+(1, 'Suse', 'Tran', 'st@mail.de', '123456'),
+(2, 'Klea', 'Grube', 'kg@mail.de', '654321'),
+(3, 'Ernst', 'Lustig', 'el@mail.de', 'pa55wort'),
+(4, 'Gernhardt', 'Reinholzen', 'gr@mail.de', 'bo$$man69'),
+(5, 'Jana', 'Türlich', 'jt@mail.de', 'naklar1'),
+(6, 'Albert', 'Rum', 'ar@mail.de', 'nurSpa55');
 
 -- --------------------------------------------------------
 
@@ -104,7 +109,7 @@ CREATE TABLE `taskarten` (
 --
 
 INSERT INTO `taskarten` (`id`, `taskart`, `taskartenicon`) VALUES
-(1, 'Todo', 'PLACEHOLDER');
+(1, 'Todo', 'fa-briefcase');
 
 -- --------------------------------------------------------
 
@@ -132,9 +137,14 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `personenid`, `taskartenid`, `spaltenid`, `sortid`, `tasks`, `erstelldatum`, `erinnerungsdatum`, `erinnerung`, `notizen`, `erledigt`, `geloescht`) VALUES
-(1, 1, 1, 1, 0, 'Prokrastinieren', '2024-01-08', '2024-02-01 00:00:00', 0, 'Muss dringen erledigt werden!', 0, 0),
+(1, 4, 1, 2, 0, 'Internet Deinstallieren', '2024-01-08', '2024-01-10 12:00:00', 0, 'Genug ist genug.', 0, 0),
 (2, 1, 1, 1, 1, 'Faulenzen', '2024-01-08', '2024-02-02 00:00:00', 1, 'Essenziell!', 0, 0),
-(3, 1, 1, 3, 0, 'Chillen', '2023-12-01', '2024-01-01 00:00:00', 0, 'Wichtig!', 1, 0);
+(3, 5, 1, 3, 0, 'Chillen', '2023-12-01', '2024-01-01 00:00:00', 0, 'Wichtig!', 1, 0),
+(4, 2, 1, 1, 0, 'Relaxen', '2024-01-10', '2024-01-10 18:59:00', 1, 'Einfach mal die Füßchen hochlegen.', 0, 0),
+(6, 6, 1, 5, 0, 'Schnee von gestern loswerden', '2024-01-10', '2024-01-10 19:00:00', 1, 'Art von Schnee: unspezifiziert.\r\nadawdawd\r\nawdqwadfawfawfaw\r\nawfaw\r\nfaw\r\nfawf\r\nawf\r\n\r\nawffwawf\r\nawf\r\naw\r\nfaw\r\nfawfawf\r\nawf\r\nawf', 0, 0),
+(7, 3, 1, 6, 0, 'Einen Task erledigen', '2024-01-10', '2024-01-10 19:00:00', 1, 'Ist erledigt!', 0, 0),
+(8, 4, 1, 4, 0, 'Einkaufliste', '2024-01-10', '2024-01-10 19:00:00', 1, 'Einkaufliste für letzte Woche schreiben.', 0, 0),
+(13, 6, 1, 3, 0, 'jep', '2024-02-03', '2024-02-03 17:29:00', 0, 'this is fine', 0, 0);
 
 --
 -- Indizes der exportierten Tabellen
@@ -182,19 +192,19 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT für Tabelle `boards`
 --
 ALTER TABLE `boards`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT für Tabelle `personen`
 --
 ALTER TABLE `personen`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT für Tabelle `spalten`
 --
 ALTER TABLE `spalten`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT für Tabelle `taskarten`
@@ -206,7 +216,7 @@ ALTER TABLE `taskarten`
 -- AUTO_INCREMENT für Tabelle `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints der exportierten Tabellen
